fd6211a995d1121ca77876900344b3c26bbbc608
daniel <daniel.huang@telecom-sudparis.eu>
2021-02-19 22:12:02 +0100

    override moves() method

--- piece.py
+++ piece.py
@@ -9,9 +9,31 @@ class Piece:
         self.board = board
         self.team = team
         self.eat = []
+        self.directions = []
 
-    def moves(self):
-        raise NotImplementedError("Subclass must implement abstract method")
+    def moves(self, continuous:bool = True):
+        self.eat = []   # reset canEat
+        moves = []
+        x0, y0 = self.position
+        for direction in self.directions:   #pour chaque direction
+            movx, movy = direction
+            while 8 > self.position[0] + movx >= 0 and 8 > self.position[1] + movy >= 0:    #on avance tant que ya pas de pièce
+                x, y = self.position[0], self.position[1]
+                newposition = (x + movx, y + movy)
+
+                if self.board[newposition] is None:     #then break when
+                    self.position = newposition
+                    moves.append(newposition)      # meeting an obstacle
+                elif self.board[newposition].team != self.team:
+                    self.eat.append(self.board[newposition])       #(either we can eat or not)
+                    moves.append(newposition)
+                    break
+                else:
+                    break
+                if not continuous:
+                    break
+            self.position = (x0, y0)
+        return moves
 
     def isValidMove(self):
         raise NotImplementedError("Subclass must implement abstract method")
@@ -54,27 +76,7 @@ class Knight(Piece):
         self.directions = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (-1, 2), (-1, -2), (1, -2)]
 
     def moves(self):
-        self.eat = []
-        moves = []
-        x0, y0 = self.position
-
-        for direction in [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (-1, 2), (-1, -2), (1, -2)]:
-            movx, movy = direction
-            if 8 > self.position[0] + movx >= 0 and 8 > self.position[1] + movy >= 0:
-                x, y = self.position[0], self.position[1]
-                newposition = (x + movx, y + movy)
-
-                if self.board[newposition] is None:
-                    self.position = newposition
-                    moves.append(newposition)
-                elif self.board[newposition].team != self.team:
-                    self.eat.append(self.board[newposition])
-                    moves.append(newposition)
-                    break
-                else:
-                    break
-            self.position = (x0, y0)
-        return moves
+        return super(Knight, self).moves(False)
 
     #Override
     def ValidMoves(self):
@@ -84,57 +86,19 @@ class Bishop(Piece):
     def __init__(self, position, board, team):
         super().__init__(position, board, team)
         self.name = 'B'
+        self.directions = [(1,1),(1,-1),(-1,1),(-1,-1)]
 
     def moves(self):
-        self.eat = []  # reset canEat
-        moves = []
-        x0, y0 = self.position
-
-        for direction in [(1,1),(1,-1),(-1,1),(-1,-1)]:
-            movx, movy = direction
-            while 8 > self.position[0] + movx >= 0 and 8 > self.position[1] + movy >= 0:
-                x, y = self.position[0], self.position[1]
-                newposition = (x + movx, y + movy)
-
-                if self.board[newposition] is None:
-                    self.position = newposition
-                    moves.append(newposition)
-                elif self.board[newposition].team != self.team:
-                    self.eat.append(self.board[newposition])
-                    moves.append(newposition)
-                    break
-                else:
-                    break
-            self.position = (x0, y0)
-        return moves
+        return super(Bishop, self).moves()
 
 class Rook(Piece):
     def __init__(self, position, board, team):
         super().__init__(position, board, team)
         self.name = 'R'
+        self.directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
     def moves(self):
-        self.eat=[]                             #reset canEat
-        moves = []
-        x0, y0 = self.position
-
-        for direction in [(0, 1), (0, -1), (1, 0), (-1, 0)]:                        #pour chaque direction
-            movx, movy = direction                                              #on avance tant que ya pas de pièce
-            while 8 > self.position[0]+movx >= 0 and 8 > self.position[1]+movy >= 0:          #puis break quand
-                x, y = self.position[0], self.position[1]                                   # on rencontre obstacle
-                newposition = (x+movx, y+movy)                                          #(qu'on peut manger ou non)
-
-                if self.board[newposition] is None:
-                    self.position = newposition
-                    moves.append(newposition)
-                elif self.board[newposition].team != self.team:
-                    self.eat.append(self.board[newposition])
-                    moves.append(newposition)
-                    break
-                else:
-                    break
-            self.position = (x0, y0)
-        return moves
+        return super(Rook, self).moves()
 
     #[(x + i, y) for i in range(1, 8 - x)] + \
      #          [(x - i, y) for i in range(1, x + 1)] + \
@@ -146,6 +110,7 @@ class King(Piece):
     def __init__(self, position, board, team):
         super().__init__(position, board, team)
         self.name = 'K'
+        self.directions = [(-1,-1)]
 
     def moves(self):
         x = self.position[0]
@@ -159,26 +124,7 @@ class Queen(Piece):
     def __init__(self, position, board, team):
         super().__init__(position, board, team)
         self.name = 'Q'
+        self.directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
 
     def moves(self):
-        self.eat = []
-        moves = []
-        x0, y0 = self.position
-
-        for direction in [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]:
-            movx, movy = direction
-            while 8 > self.position[0] + movx >= 0 and 8 > self.position[1] + movy >= 0:
-                x, y = self.position[0], self.position[1]
-                newposition = (x + movx, y + movy)
-
-                if self.board[newposition] is None:
-                    self.position = newposition
-                    moves.append(newposition)
-                elif self.board[newposition].team != self.team:
-                    self.eat.append(self.board[newposition])
-                    moves.append(newposition)
-                    break
-                else:
-                    break
-            self.position = (x0, y0)
-        return moves
+        return super(Queen, self).moves()
