2e6237420c5db418d8d6d27c142afae57e35a82b
daniel <daniel.huang@telecom-sudparis.eu>
2021-02-19 15:55:46 +0100

    correction knight and reformat

--- piece.py
+++ piece.py
@@ -20,26 +20,28 @@ class Piece:
         raise NotImplementedError("Subclass must implement abstract method")
 
     def update(self, position):
-        self.position=position
-        self.isEndangered=self.willBeEaten(position)
+        self.position = position
+        self.isEndangered = self.willBeEaten(position)
         if self.board.someone(position) is not None:
             self.board.someone(position).isDead = True
 
+
 class Pawn(Piece):
-    def __init__(self,position, board, team):
+    def __init__(self, position, board, team):
         super().__init__(position, board, team)
         self.name = 'P'
 
-class Knight(Piece):                      #Quentin
+
+class Knight(Piece):  # Quentin
     def __init__(self, position, board, team):
         super().__init__(position, board, team)
         self.name = 'N'
 
     def move(self):
-        res=[]
         x = self.position[0]
         y = self.position[1]
-        return [[x + i, x + j] for i in (-2, -1, 1, 2) for j in (-2, -1, 1, 2) if i != 2 and j != 2 and 0 <= x + i < 8 and 0 <= x + j < 8]
+        return [[x + i, y + j] for i in (-2, -1, 1, 2) for j in (-2, -1, 1, 2) if
+                i != 2 and j != 2 and 0 <= x + i < 8 and 0 <= y + j < 8]
 
 
 class Bishop(Piece):
@@ -50,13 +52,13 @@ class Bishop(Piece):
     def move(self):
         x = self.position[0]
         y = self.position[1]
-        return [[x + i, y + i] for i in range(1, 8) if 8>x+i>=0 and 8>y+i>=0] + \
-               [[x - i, y - i] for i in range(1, 8) if 8>x-i>=0 and 8>y-i>=0] + \
-               [[x + i, y - i] for i in range(1, 8) if 8>x+i>=0 and 8>y-i>=0] + \
-               [[x - i, y + i] for i in range(1, 8) if 8>x-i>=0 and 8>y+i>=0]
+        return [[x + i, y + i] for i in range(1, 8) if 8 > x + i >= 0 and 8 > y + i >= 0] + \
+               [[x - i, y - i] for i in range(1, 8) if 8 > x - i >= 0 and 8 > y - i >= 0] + \
+               [[x + i, y - i] for i in range(1, 8) if 8 > x + i >= 0 and 8 > y - i >= 0] + \
+               [[x - i, y + i] for i in range(1, 8) if 8 > x - i >= 0 and 8 > y + i >= 0]
 
 
-#Daniel
+# Daniel
 class Rook(Piece):
     def __init__(self, position, board, team):
         super().__init__(position, board, team)
@@ -65,10 +67,11 @@ class Rook(Piece):
     def move(self):
         x = self.position[0]
         y = self.position[1]
-        return [[x+i, y] for i in range(1, 8-x)] + \
-               [[x-i, y] for i in range(1, x+1)] + \
-               [[x, y+i] for i in range(1, 8-y)] + \
-               [[x, y-i] for i in range(1, y+1)]
+        return [[x + i, y] for i in range(1, 8 - x)] + \
+               [[x - i, y] for i in range(1, x + 1)] + \
+               [[x, y + i] for i in range(1, 8 - y)] + \
+               [[x, y - i] for i in range(1, y + 1)]
+
 
 class King(Piece):
     def __init__(self, position, board, team):
@@ -78,9 +81,10 @@ class King(Piece):
     def move(self):
         x = self.position[0]
         y = self.position[1]
-        return [[x+i, y] for i in (-1,1)] + \
-               [[x, y+i] for i in (-1,1)] + \
-               [[x+i, y+j] for i in (-1,1) for j in (-1,1)]
+        return [[x + i, y] for i in (-1, 1)] + \
+               [[x, y + i] for i in (-1, 1)] + \
+               [[x + i, y + j] for i in (-1, 1) for j in (-1, 1)]
+
 
 class Queen(Piece):
     def __init__(self, position, board, team):
@@ -89,4 +93,3 @@ class Queen(Piece):
 
     def move(self):
         return Rook.move() + Bishop.move()
-
